7c0bf9ee8eebac213af17fe82e8909eb75571e88
Gaurav01dec <tech.gauravverma@gmail.com>
2024-05-14 23:52:55 +0530

    night commit

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,3 +1,14 @@
-export default function NoProjectSelected(params) {
-    
+import noprojectimage from "../assets/no-projects.png"
+import Button from "./Button.jsx"
+export default function NoProjectSelected() {
+    return(
+        <div className="mt-24 text-center w-2/3">
+            <img src={noprojectimage} className="w-16 h-16 object-contain mx-auto"/>
+            <h2 className="text-xl font-bold text-stone-500 my-4">No project selected</h2>
+            <p>Select a project or get started with a new one...</p>
+            <p className="my-8">
+                <Button>Create a new project</Button>
+            </p>
+        </div>
+    )
 };
