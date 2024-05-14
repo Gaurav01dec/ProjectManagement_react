49de484b19ef4627b59539955668a0907331fbc1
Gaurav01dec <tech.gauravverma@gmail.com>
2024-05-14 23:10:17 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NewProject from "./components/NewProject.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
       <NewProject/>
     </main>
