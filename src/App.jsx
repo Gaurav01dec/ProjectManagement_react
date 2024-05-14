62747203a1f0471e06cdbf528c2d3f9689d19ea5
Gaurav01dec <tech.gauravverma@gmail.com>
2024-05-14 22:15:50 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8">
       <ProjectSidebar />
+      <NewProject/>
     </main>
   );
 }
